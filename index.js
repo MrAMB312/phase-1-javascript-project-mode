2d3b809c6a31d56813fb54fa67a1c4ff374de736
MrAMB312 <a.blomarz@gmail.com>
2025-01-13 21:07:06 -0600

    working fetch and adding db.json file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => console.log('we are connected!!!'))
+document.addEventListener('DOMContentLoaded', () => console.log('fetchData'))
+
+function fetchData() {
+    fetch()
+    .then(response => response.json())
+    .then((data) => console.log(data))
+}
