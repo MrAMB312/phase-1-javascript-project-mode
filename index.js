4d5b0471cbd7dc064278804630b997da3f5c6848
MrAMB312 <a.blomarz@gmail.com>
2025-01-19 17:20:53 -0600

    reducing more redundant code, adding loadHexData() to fetchData() to load red color data when page loads

--- index.js
+++ index.js
@@ -23,76 +23,7 @@ function fetchData() {
     .then(colorsData => {
         colors = colorsData;
 
-        const redData = colors.red;
-        
-        const h1 = document.getElementById('header');
-        
-        const nav = document.getElementById('nav');
-        const redIcon = document.getElementById('red-color-icon');
-        
-        const section1 = document.getElementById('section-1');
-        const section1subtitle = document.getElementById('section-1-subtitle');
-        
-        const section2 = document.getElementById('section-2');
-        const section2subtitle = document.getElementById('section-2-subtitle');
-        
-        const section3 = document.getElementById('section-3');
-        const section3subtitle = document.getElementById('section-3-subtitle');
-        
-        const section4 = document.getElementById('section-4');
-        const section4subtitle = document.getElementById('section-4-subtitle');
-        
-        const hexCodesDiv = document.getElementById('hex-codes-div');
-        
-        h1.style.backgroundColor = redData.base.hex;
-        h1.style.color = redData.base_without_alpha_contrasted_text.hex;
-        
-        nav.style.backgroundColor = redData.grayscale.hex;
-        redIcon.style.fontWeight = 'bold';
-        
-        section1.style.backgroundColor = redData.base.hex;
-        section1.style.color = redData.base_without_alpha_contrasted_text.hex;
-        section1subtitle.style.backgroundColor = redData.complementary.hex;
-        section1subtitle.style.color = redData.complementary_without_alpha_contrasted_text.hex;
-        
-        section2.style.backgroundColor = redData.complementary.hex;
-        section2.style.color = redData.complementary_without_alpha_contrasted_text.hex; 
-        section2subtitle.style.backgroundColor = redData.base.hex;
-        section2subtitle.style.color = redData.base_without_alpha_contrasted_text.hex;
-        
-        section3.style.backgroundColor = redData.base.hex;
-        section3.style.color = redData.base_without_alpha_contrasted_text.hex; 
-        section3subtitle.style.backgroundColor = redData.grayscale.hex;
-        section3subtitle.style.color = redData.grayscale_without_alpha_contrasted_text.hex;
-        
-        section4.style.backgroundColor = redData.complementary.hex;
-        section4.style.color = redData.complementary_without_alpha_contrasted_text.hex; 
-        section4subtitle.style.backgroundColor = redData.grayscale.hex;
-        section4subtitle.style.color = redData.grayscale_without_alpha_contrasted_text.hex;
-        
-        hexCodesDiv.style.backgroundColor = redData.base.hex;
-        hexCodesDiv.style.color = redData.base_without_alpha_contrasted_text.hex;
-        
-        const hexContainer = document.querySelector('#hex-container')
-        
-        const card = document.createElement('div');
-        card.classList.add('card');
-        
-        const p1 = document.createElement('p');
-        p1.textContent = 'Selected Color: Red';
-        
-        const p2 = document.createElement('p');
-        p2.textContent = `Base: ${redData.base.hex}`
-        
-        const p3 = document.createElement('p');
-        p3.textContent = `Complementary: ${redData.complementary.hex}`
-        
-        const p4 = document.createElement('p');
-        p4.textContent = `Grayscale: ${redData.grayscale.hex}`
-        
-        card.append(p1, p2, p3, p4);
-        
-        hexContainer.appendChild(card);
+        loadHexData('red', colors);
     })
 }
 
