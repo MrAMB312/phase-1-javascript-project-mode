2978d74804bd6d70ebb8d0c77bc94103b962af3a
MrAMB312 <a.blomarz@gmail.com>
2025-01-19 14:27:46 -0600

    adding mouseover and mouseoff events, adding default styling when page loads, adding more style.css elements

--- index.js
+++ index.js
@@ -3,70 +3,103 @@ document.addEventListener('DOMContentLoaded', () => main ())
 function fetchData() {
     fetch('http://localhost:3000/colors')
     .then(response => response.json())
-    .then(colors =>
-        Object.keys(colors).forEach(colorName => {
-            const hexContainer = document.querySelector('#hex-container')
-            const colorData = colors[colorName];
+    .then(colors => {
+        loadDefaultColors(colors);
 
-            const card = document.createElement('div');
-            card.classList.add('card');
-
-            const h3 = document.createElement('h3');
-            h3.textContent = colorName.charAt(0).toUpperCase() + colorName.slice(1);
-
-            const p1 = document.createElement('p');
-            p1.textContent = `Base: ${colorData.base.hex}`
-
-            const p2 = document.createElement('p');
-            p2.textContent = `Complementary: ${colorData.complementary.hex}`
-
-            const p3 = document.createElement('p');
-            p3.textContent = `Grayscale: ${colorData.grayscale.hex}`
-
-            card.append(h3, p1, p2, p3);
-
-            hexContainer.appendChild(card);
-        })
-    )
+        loadSelectedHexData(colors);
+    })
 }
 
-const redColorIcon = document.getElementById('red-color-icon');
-const orangeColorIcon = document.getElementById('orange-color-icon');
-const yellowColorIcon = document.getElementById('yellow-color-icon');
-const greenColorIcon = document.getElementById('green-color-icon');
-const blueColorIcon = document.getElementById('blue-color-icon');
+function loadDefaultColors(colors) {
+    const h1 = document.getElementById('header');
 
-redColorIcon.addEventListener('click', (event) => {
-    fetch('http://localhost:3000/colors')
-    .then(response => response.json())
-    .then(data => {
-        const redBaseColor = data.colors.red.base.hex;
-        const redContrastedTextColor = data.colors.red.base_without_alpha_contrasted_text.hex;
+    const nav = document.getElementById('nav');
 
-        const h1 = document.getElementsByClassName('h1');
+    const section1 = document.getElementById('section-1');
+    const section1subtitle = document.getElementById('section-1-subtitle');
 
-        h1.style.color = redBaseColor;
-        h1.textContent = redContrastedTextColor;
+    const section2 = document.getElementById('section-2');
+    const section2subtitle = document.getElementById('section-2-subtitle');
+
+    const section3 = document.getElementById('section-3');
+    const section3subtitle = document.getElementById('section-3-subtitle');
+
+    const section4 = document.getElementById('section-4');
+    const section4subtitle = document.getElementById('section-4-subtitle');
+
+    const hexCodesDiv = document.getElementById('hex-codes-div');
+
+    h1.style.backgroundColor = colors.red.base.hex;
+    h1.style.color = colors.red.base_without_alpha_contrasted_text.hex;
+
+    nav.style.backgroundColor = colors.red.grayscale.hex;
+
+    section1.style.backgroundColor = colors.red.base.hex;
+    section1.style.color = colors.red.base_without_alpha_contrasted_text.hex;
+    section1subtitle.style.backgroundColor = colors.red.complementary.hex;
+    section1subtitle.style.color = colors.red.complementary_without_alpha_contrasted_text.hex;
+
+    section2.style.backgroundColor = colors.red.complementary.hex;
+    section2.style.color = colors.red.complementary_without_alpha_contrasted_text.hex; 
+    section2subtitle.style.backgroundColor = colors.red.base.hex;
+    section2subtitle.style.color = colors.red.base_without_alpha_contrasted_text.hex;
+
+    section3.style.backgroundColor = colors.red.base.hex;
+    section3.style.color = colors.red.base_without_alpha_contrasted_text.hex; 
+    section3subtitle.style.backgroundColor = colors.red.grayscale.hex;
+    section3subtitle.style.color = colors.red.grayscale_without_alpha_contrasted_text.hex;
+
+    section4.style.backgroundColor = colors.red.complementary.hex;
+    section4.style.color = colors.red.complementary_without_alpha_contrasted_text.hex; 
+    section4subtitle.style.backgroundColor = colors.red.grayscale.hex;
+    section4subtitle.style.color = colors.red.grayscale_without_alpha_contrasted_text.hex;
+
+    hexCodesDiv.style.backgroundColor = colors.red.grayscale.hex;
+    hexCodesDiv.style.color = colors.red.grayscale_without_alpha_contrasted_text.hex;
+}
+
+function loadHexData(colors) {
+    Object.keys(colors).forEach(colorName => {
+        const hexContainer = document.querySelector('#hex-container')
+        const colorData = colors[colorName];
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const h3 = document.createElement('h3');
+        h3.textContent = colorName.charAt(0).toUpperCase() + colorName.slice(1);
+
+        const p1 = document.createElement('p');
+        p1.textContent = `Base: ${colorData.base.hex}`
+
+        const p2 = document.createElement('p');
+        p2.textContent = `Complementary: ${colorData.complementary.hex}`
+
+        const p3 = document.createElement('p');
+        p3.textContent = `Grayscale: ${colorData.grayscale.hex}`
+
+        card.append(h3, p1, p2, p3);
+
+        hexContainer.appendChild(card);
+
     })
-})
-
-orangeColorIcon.addEventListener('click', (event) => {
-
-})
-
-yellowColorIcon.addEventListener('click', (event) => {
-
-})
-
-greenColorIcon.addEventListener('click', (event) => {
-
-})
-
-blueColorIcon.addEventListener('click', (event) => {
-
-})
-
+}
 
 function main() {
     fetchData();
+
+    document.querySelectorAll('.color-icon').forEach(icon => { 
+        icon.addEventListener('mouseover', (event) => {
+            event.target.style.color = 'white';
+        })
+
+        icon.addEventListener('mouseout', (event) => {
+            event.target.style.color = '';
+        })
+        
+        icon.addEventListener('click', (event) => {
+            // load colors to respective containers [FETCH]
+            
+        })
+    })
 }
